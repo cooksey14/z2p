56ec8d1d33d90d4786ff9d6a434d590ca5966f52
Colin Cook <cook.colin13@gmail.com>
2021-07-04 08:05:41 -0500

    added health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
@@ -16,3 +21,4 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
+
